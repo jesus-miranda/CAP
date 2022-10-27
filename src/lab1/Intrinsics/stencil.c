6abd57d797b9b54a0f98d694c47a39ef465966cd
Jesús Miranda <jmirandamejia@hotmail.com>
2022-10-27 09:51:52 -0500

    Update stencil.c
    Cambio de variables

--- src/lab1/Intrinsics/stencil.c
+++ src/lab1/Intrinsics/stencil.c
@@ -3,7 +3,8 @@
 #include <immintrin.h>
 #include "stencil.h"
 
-void ApplyStencil(unsigned char *img_in, unsigned char *img_out, int width, int height) {
+void ApplyStencil(unsigned char *img_in, unsigned char *img_out, int width, int height) 
+{
 	
 	//Caracter sin signo
 	unsigned char val_out;
@@ -12,50 +13,53 @@ void ApplyStencil(unsigned char *img_in, unsigned char *img_out, int width, int 
 	int i, j;
 	
 	//Variables de transicion para almacenar los datos
-	__m256i val, m1, m2, m3, m4, m5, m6, m7, m8, a;
+	__m256i valor, nodo1, nodo2, nodo3, nodo4, nodo5, nodo6, nodo7, nodo8, a;
 
-	for (i = 1; i < height-1; i++) {
-		for (j = 1; j < width-1; j+=8) {
+	for (i = 1; i < height-1; i++) 
+	{
+		for (j = 1; j < width-1; j+=8) 
+		{
 			
             //Carga de la imagen
 			a = _mm256_loadu_si256((const __m256i *) &img_in[(i ) * width + j]);
-			m1 = _mm256_loadu_si256((const __m256i *) &img_in[(i-1) * width + j-1]);
-			m2 = _mm256_loadu_si256((const __m256i *) &img_in[(i-1) * width + j]);
-			m3 = _mm256_loadu_si256((const __m256i *) &img_in[(i-1) * width + j+1]);
-			m4 = _mm256_loadu_si256((const __m256i *) &img_in[(i  ) * width + j-1]);
-			m5 = _mm256_loadu_si256((const __m256i *) &img_in[(i  ) * width + j+1]);
-			m6 = _mm256_loadu_si256((const __m256i *) &img_in[(i+1) * width + j-1]);
-			m7 = _mm256_loadu_si256((const __m256i *) &img_in[(i+1) * width + j]);
-			m8 = _mm256_loadu_si256((const __m256i *) &img_in[(i+1) * width + j+1]);
+			nodo1 = _mm256_loadu_si256((const __m256i *) &img_in[(i-1) * width + j-1]);
+			nodo2 = _mm256_loadu_si256((const __m256i *) &img_in[(i-1) * width + j]);
+			nodo3 = _mm256_loadu_si256((const __m256i *) &img_in[(i-1) * width + j+1]);
+			nodo4 = _mm256_loadu_si256((const __m256i *) &img_in[(i  ) * width + j-1]);
+			nodo5 = _mm256_loadu_si256((const __m256i *) &img_in[(i  ) * width + j+1]);
+			nodo6 = _mm256_loadu_si256((const __m256i *) &img_in[(i+1) * width + j-1]);
+			nodo7 = _mm256_loadu_si256((const __m256i *) &img_in[(i+1) * width + j]);
+			nodo8 = _mm256_loadu_si256((const __m256i *) &img_in[(i+1) * width + j+1]);
 
             //Operaciones entre los datos, resta
-			val = _mm256_subs_epu8(a, m1);
-			val = _mm256_subs_epu8(val, m2);
-			val = _mm256_subs_epu8(val, m3);
-			val = _mm256_subs_epu8(val, m4);
-			val = _mm256_subs_epu8(val, m5);
-			val = _mm256_subs_epu8(val, m6);
-			val = _mm256_subs_epu8(val, m7);
-			val = _mm256_subs_epu8(val, m8);
+			valor = _mm256_subs_epu8(a, nodo1);
+			valor = _mm256_subs_epu8(valor, nodo2);
+			valor = _mm256_subs_epu8(valor, nodo3);
+			valor = _mm256_subs_epu8(valor, nodo4);
+			valor = _mm256_subs_epu8(valor, nodo5);
+			valor = _mm256_subs_epu8(valor, nodo6);
+			valor = _mm256_subs_epu8(valor, nodo7);
+			valor = _mm256_subs_epu8(valor, nodo8);
 
 			//Operaciones entre los datos, suma
-            //Esta operacion deberiamos buscar una manera de realizarlo en una unica operacion sin generar dependencia de val con su valor anterior
-			val = _mm256_adds_epu8(val, a);
-			val = _mm256_adds_epu8(val, a);
-			val = _mm256_adds_epu8(val, a);
-			val = _mm256_adds_epu8(val, a);
-			val = _mm256_adds_epu8(val, a);
-			val = _mm256_adds_epu8(val, a);
-			val = _mm256_adds_epu8(val, a);
+            //Esta operacion deberiamos buscar una manera de realizarlo en una unica operacion sin generar dependencia de valor con su valor anterior
+			valor = _mm256_adds_epu8(valor, a);
+			valor = _mm256_adds_epu8(valor, a);
+			valor = _mm256_adds_epu8(valor, a);
+			valor = _mm256_adds_epu8(valor, a);
+			valor = _mm256_adds_epu8(valor, a);
+			valor = _mm256_adds_epu8(valor, a);
+			valor = _mm256_adds_epu8(valor, a);
 
             //Escritura del dato
-			_mm256_storeu_si256((__m256i *)&img_out[i*width + j], val);
+			_mm256_storeu_si256((__m256i *)&img_out[i*width + j], valor);
 
 		}
 	}
 }
 
-void CopyImage(unsigned char *img_in, unsigned char *img_out, int width, int height) {
+void CopyImage(unsigned char *img_in, unsigned char *img_out, int width, int height) 
+{
 	//Declaracion de i/j fuera del bucle for
 	int i, j;
 	
