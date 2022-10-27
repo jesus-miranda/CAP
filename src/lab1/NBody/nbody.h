7e003a3f3a7f41e28b493e6443f1426b504d5fb7
Jesús Miranda <jmirandamejia@hotmail.com>
2022-10-27 16:51:55 -0500

    Update nbody.h
    mejoras nbody

--- src/lab1/NBody/nbody.h
+++ src/lab1/NBody/nbody.h
@@ -1,6 +1,14 @@
 #ifndef NBODY_H
 #define NBODY_H
 
-typedef struct { float m, x, y, z, vx, vy, vz; } body;
+typedef struct { 
+float *m;
+float *x;
+float *y;
+float *z;
+float *vx;
+float *vy;
+float *vz; 
+} body;
 
 #endif
