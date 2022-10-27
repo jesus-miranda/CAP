dfa0fdbdb7520c7e30be17e86e6f502eb94bec2b
Jesús Miranda <jmirandamejia@hotmail.com>
2022-10-27 16:51:32 -0500

    Update nbody_routines.c
    mejoras Nbody

--- src/lab1/NBody/nbody_routines.c
+++ src/lab1/NBody/nbody_routines.c
@@ -1,7 +1,9 @@
 #include <math.h>
 #include "nbody.h"
 
-void bodyForce(body *p, float dt, int n) {
+void bodyForce(body p, float dt, int n) {
+
+const int alignment=64;
 
 	for (int i = 0; i < n; i++) { 
 		float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
@@ -9,18 +11,18 @@ void bodyForce(body *p, float dt, int n) {
 		#pragma vector alligned
 		for (int j = 0; j < n; j++) {
 			if (i!=j) {
-				float dx = p[j].x - p[i].x;
-				float dy = p[j].y - p[i].y;
-				float dz = p[j].z - p[i].z;
+				const float dx = p.x[j] - p.x[i];
+				const float dy = p.y[j] - p.y[i];
+				const float dz = p.z[j] - p.z[i];
 				//double softeningSquared = 1e-3;
-				float softeningSquared = 1e-3;
-				float distSqr = dx*dx + dy*dy + dz*dz + softeningSquared;
-				float invDist = 1.0f / sqrtf(distSqr);
-				float invDist3 = invDist * invDist * invDist;
+				const float softeningSquared = 1e-3;
+				const float distSqr = dx*dx + dy*dy + dz*dz + softeningSquared;
+				const float invDist = 1.0f / sqrtf(distSqr);
+				const float invDist3 = invDist * invDist * invDist;
 
 				//double G = 6.674e-11;
-				float G = 6.674e-11;
-				float g_masses = G * p[j].m * p[i].m;
+				const float G = 6.674e-11;
+				const float g_masses = G * p.m[j] * p.m[i];
 
 				Fx += g_masses * dx * invDist3; 
 				Fy += g_masses * dy * invDist3; 
@@ -28,14 +30,14 @@ void bodyForce(body *p, float dt, int n) {
 			}
 		}
 
-		p[i].vx += dt*Fx/p[i].m; p[i].vy += dt*Fy/p[i].m; p[i].vz += dt*Fz/p[i].m;
+		p.vx[i] += dt*Fx/p.m[i]; p.vy[i] += dt*Fy/p.m[i]; p.vz[i] += dt*Fz/p.m[i];
 	}
 }
 
-void integrate(body *p, float dt, int n){
+void integrate(body p, float dt, int n){
 	for (int i = 0 ; i < n; i++) {
-		p[i].x += p[i].vx*dt;
-		p[i].y += p[i].vy*dt;
-		p[i].z += p[i].vz*dt;
+		p.x[i] += p.vx[i]*dt;
+		p.y[i] += p.vy[i]*dt;
+		p.z[i] += p.vz[i]*dt;
 	}
 }
