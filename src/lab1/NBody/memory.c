4d84ed978b05a0058c3231ff93c7c2ea405b1a47
Jesús Miranda <jmirandamejia@hotmail.com>
2022-10-27 16:50:46 -0500

    Update memory.c
    Mejoras nbody

--- src/lab1/NBody/memory.c
+++ src/lab1/NBody/memory.c
@@ -3,28 +3,23 @@
 #include <stdlib.h>
 #include "nbody.h"
 
-body* get_memory(int nBodies)
+
+
+void free_memory(body* p)
 {
-	body *p = (body*)malloc(nBodies*sizeof(body));
-	return p;
+	_mm_free(p);
 }
 
-void free_memory(body *p)
-{
-	free(p);
-}
-
-void randomizeBodies(body *data, int n) {
+void randomizeBodies(body* data, int n) {
 	for (int i = 0; i < n; i++) {
-		data[i].m  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data.m[i]  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
 
-		data[i].x  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-		data[i].y  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-		data[i].z  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data.x[i]  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data.y[i]  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data.z[i]  = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
 
-		data[i].vx = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-		data[i].vy = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-		data[i].vz = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data.vx[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data.vy[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+		data.vz[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
 	}
 }
-
